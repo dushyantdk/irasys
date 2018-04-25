62ac535ea8b7a0cd8a7bdc418dccc994f329a8ae
dushyantdk <dushyantcreate@gmail.com>
2018-04-25 18:09:05 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -48,6 +48,9 @@ function setClassAccordingToPage() {
    } else if(html_page == 'projects') {
        $('body').attr('class', '');
        $('body').addClass('projects');
+   }else if(html_page == 'case-study') {
+       $('body').attr('class', '');
+       $('body').addClass('case-study');
    } else if(html_page == 'contact') {
        $('body').attr('class', '');
        $('body').addClass('contact');
@@ -114,3 +117,81 @@ $(document).on('click','#home_mouse',function(){
    $('head').append('<script>new WOW().init();</script>');
 });
 
+
+
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if($(window).width() > 990){
+      if (scroll >= 750) {
+          $('.navbar').addClass('navbar-fixed-top');
+          // $('.header_logo').removeClass('is-hidden');
+          // //$(".top_header").hide();
+          // $(".navbar-brand img").show(50);
+      } else {
+          $('.navbar').removeClass('navbar-fixed-top');
+          // $('.header_logo').addClass('is-hidden');
+          // //$(".top_header").show();
+          // $(".navbar-brand img").hide(50);
+      }
+    }
+
+});
